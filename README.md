2ef3c9c0e74bc0b74725819ba313a3197375298b
Kevin Huynh <kevinhuynh1@gmail.com>
2021-05-09 19:13:46 -0700

    Command line argument parsing and read from stdin

--- README.md
+++ README.md
@@ -1,18 +1,53 @@
-Time Butler: A CLI tool for easy datetime conversions
+Time Butler: A CLI tool for easy datetime conversions to
 =====================================================
 ![Build Status](https://travis-ci.com/heepster/time-butler.svg?branch=main)
 
-⚠️ Under Active Development ⚠️
+⚠️ Under Development ⚠️
+
+> “There is no future. There is no past. Do you see? Time is simultaneous, an intricately structured jewel that humans insist on viewing one edge at a time, when the whole design is visible in every facet.” - Dr. Manhanttan
 
 Time Butler converts datetimes of various formats into your local time.
 
+## Build
+
+```
+cargo build --release
+```
+This will create a binary called `tb` in `./target/release/`
 
 ## Usage
 
 ```
-cargo run 1620601103
-=> 2021-05-09T15:58:23-07:00
+USAGE:
+    tb [FLAGS] [arg_datetime]
 
-cargo run "2021-05-09T23:03:18+00:00"
+ARGS:
+    <arg_datetime>    Datetime to parse
+
+FLAGS:
+    -h, --help       Prints help information
+    -i, --stdin      Read from stdin
+    -V, --version    Prints version information
+```
+
+Pass an EPOCH timestamp as an argument:
+```
+tb 1620601103
+=> 2021-05-09T15:58:23-07:00
+```
+
+Pass a standard ISO8601 timestamp as an argument:
+```
+tb "2021-05-09T23:03:18+00:00"
 => 2021-05-09T16:03:18-07:00
 ```
+
+Read from STDIN.
+```
+echo 1620601103 | tb -i
+=> 2021-05-09T15:58:23-07:00
+```
+
+## Roadmap
+* Continously pipe into stdin and transform only datetimes
+* More parseable datetime formats
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,50 @@
-use std::env;
+use std::io;
+use clap::{App, AppSettings, Arg};
 
 mod lib;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+    const AUTHOR: &'static str = env!("CARGO_PKG_AUTHORS");
 
-    let time_str = &args[1];
+    let matches = App::new("Time Butler")
+        .global_setting(
+          AppSettings::ColoredHelp
+        )
+        .version(VERSION)
+        .author(AUTHOR)
+        .about("A tool for easy datetime conversions")
+        .arg(
+            Arg::new("arg_datetime")
+                .about("Datetime to parse")
+                .index(1)
+        )
+        .arg(
+            Arg::new("stdin")
+                .short('i')
+                .long("stdin")
+                .about("Read from stdin")
+        )
+        .get_matches();
 
-    std::process::exit(match lib::date_parse(time_str) {
+    let time_str;
+
+    if matches.is_present("stdin") {
+        let mut stdin_str = String::new();
+        if let Ok(_) = io::stdin().read_line(&mut stdin_str) {
+            time_str = stdin_str
+        } else {
+            println!("Couldn't read from stdin");
+            std::process::exit(1);
+        }
+    } else if let Some(o) = matches.value_of("arg_datetime") {
+        println!("Must pass datetime as an argument");
+        time_str = o.to_string();
+    } else {
+        std::process::exit(1);
+    }
+
+    std::process::exit(match lib::date_parse(&time_str) {
         Ok(local_time) => {
             println!("{:?}", local_time);
             0
